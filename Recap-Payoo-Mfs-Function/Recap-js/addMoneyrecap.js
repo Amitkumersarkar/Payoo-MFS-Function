73b5d42877c836b56c8d0325d622ab7f11bfc2bf
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-10 01:00:14 +0600

    added new function file

--- Recap-Payoo-Mfs-Function/Recap-js/addMoneyrecap.js
+++ Recap-Payoo-Mfs-Function/Recap-js/addMoneyrecap.js
@@ -1,8 +1,22 @@
 document.getElementById('btn-add-money').addEventListener('click', function (event) {
     event.preventDefault();
+    console.log('add money button click');
+    // here we call function from utilities 
+    // function call from separated file
+    // getInputFieldValueById();
+
+    // const addMoney = getInputFieldValueById();
+    // console.log('add money value', addMoney);
+
     // const addMoney = document.getElementById('input-add-money').value;
     // const addMoneyNumber = parseFloat(addMoney);
 
-    
+    // call utilities function
+
+    const addMoney = getInputFieldValueById('input-add-money');
+
+    const pinNumber = getInputFieldValueById('input-pin-number');
+
+    console.log('add money with parameter', addMoney, pinNumber);
 
 })
--- Recap-Payoo-Mfs-Function/Recap-js/utilities.js
+++ Recap-Payoo-Mfs-Function/Recap-js/utilities.js
@@ -1,4 +1,14 @@
-// common shared function we used 
-function getInputFieldValueById() {
-    console.log('will get value by id');
-};
+// common shared function we used
+// function getInputFieldValueById() {
+//     console.log('will get value by id');
+//     const addMoney = document.getElementById('input-add-money').value;
+//     // return value into main file
+//     return addMoney;
+// };
+
+// create new function
+
+function getInputFieldValueById(id) {
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
+}
